3102c352710f7cde4afd0d783e4dba233c54c402
rodrigolsantoswk <limasrodrigowk@gmail.com>
2024-02-15 20:26:52 -0300

    atualizando projeto com conexão ao database

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const http = require('http')
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
-const hostname= '127.0.0.1';
-
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req,res)=> {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain'); 
-    res.end('Hello world!');
+// Configuração da conexão com o banco de dados usando variáveis de ambiente
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Necessário para evitar erros de certificado SSL
+  }
 });
 
-server.listen(port,hostname,() => {
-    console.log('Server running!');
+// Rota para obter todos os registros da tabela 'exemplo'
+app.get('/exemplo', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM aluno');
+    res.json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error('Erro ao buscar dados:', err);
+    res.status(500).json({ error: 'Erro ao buscar dados' });
+  }
+});
+
+// Inicie o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
